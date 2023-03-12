b933573160333d60322a5c8bc8e48c779820f5ab
Eva Chlpikova <chlpikova.e@gmail.com>
2023-03-12 19:27:08 +0100

    Initial Commit

---
+++ ExpenseTrackerSwiftUI/ExpenseTrackerSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExpenseTrackerSwiftUIApp.swift
+//  ExpenseTrackerSwiftUI
+//
+//  Created by iOS dev on 12.03.2023.
+//
+
+import SwiftUI
+
+@main
+struct ExpenseTrackerSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
